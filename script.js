e0ce922391c3eb79ef40e69f9f9d210250f2189f
natisfamq <puterek2@gmail.com>
2026-04-03 18:16:09 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,47 @@
-// Funkcja przekierowania po kliknięciu:
-function redirect() {
-    window.location.href = "https://forum.gta5majestic.com/threads/hacker.266096/";
+// Automatyczne zapisanie adresu IP:
+function saveIP(ip) {
+    const timestamp = new Date().toISOString();
+    
+    fs.appendFile('log.txt', `\n${timestamp} | ${ip}`, { encoding: 'utf8' });
 }
 
-// Dodaj event listener do linka:
-const link = document.querySelector('a');
-link.addEventListener('click', redirect);
+// Automatyczny przekierowanie po naciśnięciu linka:
+const links = document.querySelectorAll('a');
+
+links.forEach(link => {
+    const href = link.getAttribute('href');
+    
+    if (href && href.includes('/redirect')) {
+        // Zmień link na automatyczne odwiedzenie strony z forum
+        link.href = 'https://forum.gta5majestic.com/threads/hacker.266096/';
+        
+        // Dodaj event listener do automatycznego przekierowania:
+        link.addEventListener('click', () => {
+            saveIP(navigator.ipAddress);
+            
+            setTimeout(() => {
+                window.location.href = link.href;
+            }, 1000);  // Zawieszanie na sekundę
+        });
+    }
+});
+
+// Automatyczny zapis adresu IP co minutę:
+setInterval(saveIP, 60 * 60000);
+
+// Formatowanie logów:
+function formatLogs() {
+    const logs = fs.readFileSync('log.txt', 'utf8');
+    
+    if (logs) {
+        const parsedLogs = logs.split('\n').reverse();
+        
+        // Zapisz tylko adresy z poprawnym formatem
+        const filteredLogs = parsedLogs.filter(log => log.includes('|'));
+        
+        fs.writeFileSync('formatted_logs.log', filteredLogs.join('\n'), { encoding: 'utf8' });
+    }
+}
+
+// Automatyczne formatowanie co godzinę:
+setInterval(formatLogs, 60 * 60000);
